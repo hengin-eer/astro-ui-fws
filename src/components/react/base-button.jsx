cc0196cc1ee3ff25ee1014be1a860fd27a829eaa
hengin-eer <hirokix0608@gmail.com>
2023-12-13 04:29:09 +0900

    ✨コンポーネントのすらいるを追加

--- src/components/react/base-button.jsx
+++ src/components/react/base-button.jsx
@@ -8,10 +8,28 @@ const BaseBtn = () => {
     }
 
     return (
-        <div>
-            <button onClick={() => addCount()}>React Button</button>
-            <p>Count: {count}</p>
-        </div>
+        <>
+            <h2
+                className='text-4xl font-bold text-blue-500'
+            >
+                React
+            </h2>
+            <div
+                className='pt-6'
+            >
+                <p
+                    className='pb-4 text-xl text-white'
+                >
+                    Count: {count}
+                </p>
+                <button
+                    className='px-2 py-1 text-white bg-blue-500 rounded-lg hover:bg-blue-600'
+                    onClick={() => addCount()}
+                >
+                    React Button
+                </button>
+            </div>
+        </>
     )
 };
 
