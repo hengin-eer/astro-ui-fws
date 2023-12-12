53f80aead959f130f38aeae1c178dcebd57aed73
hengin-eer <hirokix0608@gmail.com>
2023-12-13 08:17:10 +0900

    ✨Svelteでコンポーネントを作成

---
+++ src/components/svelte/svelte-button.svelte
@@ -0,0 +1,20 @@
+<script>
+    let count = 0;
+    function addCount() {
+        count += 1;
+    }
+</script>
+
+<div class="flex flex-col">
+    <h2 class="text-4xl font-bold text-orange-500">
+        Svelte
+    </h2>
+    <div class="pt-6">
+        <p class="pb-4 text-xl text-white">
+            Count: {count}
+        </p>
+        <button class="px-2 py-1 text-white bg-orange-500 rounded-lg hover:bg-orange-600" on:click={addCount}>
+            Svelte Button
+        </button>
+    </div>
+</div>
